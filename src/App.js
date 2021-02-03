8adf1fdd0e063a338d887564a1cd8c22158ebcf2
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-02-03 18:56:47 +0500

    feat: basic skelton for project would you rather added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react'
 import './App.css';
 import Login from './components/Login'
 
-function App() {
-  return (
-    <Login />
-  )
+class App extends React.Component {
+
+  render () {
+    return (
+      <Login />
+    )
+  }
+
 }
 
-export default App;
+export default App
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { loadUsers } from '../actions/users'
+import { connect } from 'react-redux'
+
 
 class Login extends React.Component {
     state = {
         selectedUser: ""
+    }
+
+
+    componentDidMount() {
+    this.props.dispatch(loadUsers())
     }
 
     handleChange = (e) => {
@@ -23,8 +31,9 @@ class Login extends React.Component {
                 <div>
                     Please select your name
                     <select value={this.state.selectedUser} onChange={this.handleChange}>
-                        <option>Tyler</option>
-                        <option>Tasawer</option>
+                        {this.props.users.map(user =>
+                            <option key={user.id}>{user.name}</option>
+                        )}
                     </select>
                     <button type="submit" onClick={this.handleSubmit}>Login</button>
                 </div>
@@ -33,4 +42,10 @@ class Login extends React.Component {
     }
 }
 
-export default Login
+function mapStateToProps({users}) {
+    return {
+        users: Object.values(users)
+    }
+}
+
+export default connect(mapStateToProps)(Login)
