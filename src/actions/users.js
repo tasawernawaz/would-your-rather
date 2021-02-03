43bfc111c1becc18afde7d44f6ebfaad8801e83f
tasawernawaz <tasawer.nawaz@arbisoft.com>
2021-02-03 18:55:40 +0500

    feat: add basic actions structure

---
+++ src/actions/users.js
@@ -0,0 +1,20 @@
+import {getUsers} from '../utils/api'
+
+export const RECEIVE_USERS = "RECEIVE_USERS"
+
+
+export function receiveUsers(users) {
+    return {
+        type: RECEIVE_USERS,
+        users
+    }
+}
+
+export function loadUsers() {
+    return (dispatch) => {
+        return getUsers().then((users) => {
+            dispatch(receiveUsers(users))
+        })
+
+    }
+}
